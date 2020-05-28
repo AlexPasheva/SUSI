89bec94b22850e009462238646f267c04dae5b23
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2020-05-28 21:52:41 +0300

    Update Student.h

--- Student.h
+++ Student.h
@@ -8,57 +8,73 @@ enum Status
 {
 	enrolled, dropout, graduated
 };
+enum Major
+{
+	ComputerScience, Informatics, InforamtionSystems, AppliedMathematics
+};
 class Student
 {
 private:
 	char* name;
 	unsigned int FacultyNumber;
-	unsigned int CurrentCourse;
-	char major[50];
+	unsigned int SemesterCourse;
+	Major major;
 	unsigned int group;
 	Status status;
 	double AverageGrade;
-	unsigned int NumberOfDisciplines;
-	AcademicDiscipline* Disciplines;
 
 	void CopyFrom(const Student& other);
 	void Free();
 public:
 
-	Student(const char* name, unsigned int FacultyNumber, unsigned int CurrentCourse, const  char* major, unsigned int group, Status status, double AverageGrade, unsigned int NumberOfDisciplines, AcademicDiscipline* Disciplines);
+	Student(const char* name, unsigned int FacultyNumber, unsigned int SemesterCourse, Major major, unsigned int group, Status status, double AverageGrade);
 	Student(const Student& other);
 	Student& operator=(const Student& other);
 	~Student();
 
-	//void Print(ostream& strm);
+	char* GetName();
+	unsigned int GetFacultyNumber();
+	unsigned int GetSemesterCourse();
+	Major GetMajor();
+	unsigned int GetGroup();
+	Status GetStatus();
+	double GetAverageGrade();
+
+	void SetName(char* name);
+	void SetCurrentCourse(unsigned int CurrentCourse);
+	void SetMajor(Major major);
+	void SetGroup(unsigned int group);
+	void SetStatus(Status status);
+	void SetAverageGrade(double AverageGrade);
+	void Print();
 };
-//////<BigFour>
+
+
 void Student::CopyFrom(const Student& other)
 {
 	name = new char[strlen(other.name) + 1];
 	strcpy(name, other.name);
-	strcpy(major, other.major);
-	this->Disciplines = new AcademicDiscipline[other.NumberOfDisciplines];
-	for (int i = 0; i < other.NumberOfDisciplines; i++)
-	{
-		this->Disciplines[i] = other.Disciplines[i];
-	}
+	this->major= other.major;
 }
 void Student::Free()
 {
 	delete[] name;
 }
-Student::Student(const char* name, unsigned int FacultyNumber, unsigned int CurrentCourse, const  char* major, unsigned int group, Status status, double AverageGrade, unsigned int NumberOfDisciplines, AcademicDiscipline* Disciplines)
+
+Student::Student(const char* name, unsigned int FacultyNumber, unsigned int SemesterCourse, Major major, unsigned int group, Status status, double AverageGrade)
 {
 	this->name = new char[strlen(name) + 1];
+	strcpy(this->name, name);
 	this->FacultyNumber = FacultyNumber;
-	this->CurrentCourse = CurrentCourse;
-	strcpy(this->major, major);
+	if (SemesterCourse > 0 && SemesterCourse < 5)
+	{
+		this->SemesterCourse = SemesterCourse;
+	}
+
+	this->major = major;
 	this->group = group;
 	this->status = status;
 	this->AverageGrade = AverageGrade;
-	this->NumberOfDisciplines = NumberOfDisciplines;
-	this->Disciplines = new AcademicDiscipline[NumberOfDisciplines];
 }
 Student::Student(const Student& other)
 {
@@ -77,4 +93,89 @@ Student::~Student()
 {
 	Free();
 }
-//////</BigFour>
+
+char* Student::GetName()
+{
+	return name;
+}
+unsigned int Student::GetFacultyNumber()
+{
+	return FacultyNumber;
+}
+unsigned int Student::GetSemesterCourse()
+{
+	return SemesterCourse;
+}
+Major Student::GetMajor()
+{
+	return major;
+}
+unsigned int Student::GetGroup()
+{
+	return group;
+}
+Status Student::GetStatus()
+{
+	return status;
+}
+double Student::GetAverageGrade()
+{
+	return AverageGrade;
+}
+
+void Student::SetName(char* name)
+{
+	this->name = new char[strlen(name) + 1];
+	strcpy(this->name, name);
+}
+void Student::SetCurrentCourse(unsigned int SemesterCourse)
+{
+	if (SemesterCourse > 0 && SemesterCourse < 5)
+	{
+		this->SemesterCourse = SemesterCourse;
+	}
+}
+void Student::SetMajor(Major major)
+{
+	this->major= major;
+}
+void Student::SetGroup(unsigned int group)
+{
+	this->group = group;
+}
+void Student::SetStatus(Status status)
+{
+	this->status = status;
+}
+void Student::SetAverageGrade(double AverageGrade)
+{
+	this->AverageGrade = AverageGrade;
+}
+void Student::Print()
+{
+	cout << "name: " << name << endl;
+	cout << "faulty number: " << FacultyNumber << endl;
+	cout << "semester course: " << SemesterCourse << endl;
+	cout << "major: ";
+	if (major==Major::AppliedMathematics)
+	{
+		cout << "Applied Mathematics";
+	}
+	else if(major==Major::ComputerScience)
+	{
+		cout << "Computer Science";
+	}
+	else if (major == Major::Informatics)
+	{
+		cout << "Informatics";
+	}
+	else if (major == Major::InforamtionSystems)
+	{
+		cout << "Inforamtion Systems";
+	}
+	cout << endl;
+	cout << "group: " << group << endl;
+	cout << "status: " << status << endl;
+	cout << "average grade: " << AverageGrade;
+
+}
