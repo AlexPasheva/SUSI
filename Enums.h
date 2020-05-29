cae59bc4b22d091824e2aebbbf377a02f0127ec6
Alexandra Pasheva <64726720+AlexPasheva@users.noreply.github.com>
2020-05-29 22:32:37 +0300

    Create Enums.h

---
+++ Enums.h
@@ -0,0 +1,15 @@
+#pragma once
+enum class Status
+{
+	enrolled, dropout, graduated
+};
+
+enum class Type
+{
+	mandatory, optional
+};
+
+enum MajorName
+{
+	ComputerScience, Informatics, InforamtionSystems, AppliedMathematics
+};
